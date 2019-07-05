c0ca390deaf1368d3a2c361ab02d539880726ca7
Ben Wilson <benjamin.wilson-1@colorado.edu>
2019-07-05 15:57:29 -0400

    added UserDAO and UserDAOImpl

---
+++ project0/src/main/java/com/revature/daoimpl/UserDAOImpl.java
@@ -0,0 +1,21 @@
+package com.revature.daoimpl;
+
+import java.util.List;
+
+import com.revature.beans.User;
+import com.revature.dao.UserDAO;
+
+public class UserDAOImpl implements UserDAO
+{
+
+	public void createUser(String firstName, String lastName, String username, String passw0rd, int titleNum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<User> readAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
